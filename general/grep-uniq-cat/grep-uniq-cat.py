cdccffbba4d9add5e2fa8dee5171e1fd2572ab3f
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-16 12:21:39 +0800

    add : grep-uniq-content

---
+++ general/grep-uniq-cat/grep-uniq-cat.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+sys.path.append(".")
+
+import pwn
+from util import *
+
+cur_dir = os.path.dirname(__file__).replace("\\", "/")
+cur_dir = "." + cur_dir.split("0xdeadbeef CTF")[-1]
+
+remote = pwn.remote(host="0.cloud.chals.io", port=16079)
+
+output = remote.recvall()
+output = output.decode()
+
+with open(cur_dir+"/content.txt", "w+") as f:
+    f.write(output)
+
+out, err = run_bash(["cd", cur_dir, "&&", "cat", "content.txt", "|", "sort", "|", "uniq", "-u", "|", "grep", "^elio{"])
+
+dir = save_flag(out, __file__)
